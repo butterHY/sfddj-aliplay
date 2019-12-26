421967b590a379950515c197c37ebb3a2b79ba05
zhangping <80002636@sf-express.com>
2019-12-26 13:48:25 +0800

    绑定手机号授权失败提示优化

--- sfddj-miniApp-ali/pages/shopping/goodsDetail/goodsDetail.js
+++ sfddj-miniApp-ali/pages/shopping/goodsDetail/goodsDetail.js
@@ -2011,6 +2011,24 @@ Page({
 		});
 	},
 
+
+	// 获取手机号失败
+	onAuthError(res) {
+		// my.showToast({
+		// 	content: "授权失败"
+		// })
+		let that = this
+		this.setData({
+			showToast: true,
+			showToastMes: '授权失败'
+		})
+		setTimeout(function(){
+			that.setData({
+				showToast: false
+			})
+		},2000)
+	},
+
   // 价格和积分的设置
   goodsPrice() {
       let that = this;
