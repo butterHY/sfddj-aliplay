0a2712c5a4daf9ebaa957888de3f268aab142cd8
黄家凯 <80004009@sfmail.sf-express.com>
2020-03-12 19:37:49 +0800

    1，完成支付宝小程序轻会员未注册页面头部样式和优惠券样式；2，完成支付宝小程序轻会员已注册页面头部样式并融合到未注册页面中；

--- sfddj-miniApp-ali/pages/activities/lightMember/lightMember.js
+++ sfddj-miniApp-ali/pages/activities/lightMember/lightMember.js
@@ -1,4 +1,13 @@
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    isLightMember: false
+  },
+  onLoad() {
+    my.setNavigationBar({
+      title: '会员中心',
+      backgroundColor: '#2b2d3e',
+      // borderBottomColor,
+      // image,
+    })
+  },
 });
