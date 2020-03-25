ec4a6b0c39b5cb7eefa01dd192f94a1f356f1a80
caoyongfeng0214 <caoyongfeng0214@gmail.com>
2020-03-26 01:19:36 +0800

    component => GoodsItem

---
+++ sfddj-miniApp-ali/community/pages/components/GoodsItem/GoodsItem.js
@@ -0,0 +1,14 @@
+// 商品列表中的某一项
+// 接收一个 data 属性参数，即要展示的商品的数据。
+
+Component({
+  mixins: [],
+  data: {},
+  props: {
+    data: {}
+  },
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+});
