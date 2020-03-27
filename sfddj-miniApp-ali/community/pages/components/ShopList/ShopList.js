97ce6f258245336d92bed22e82c08580346bcb75
caoyongfeng <HxKgnx7saX>
2020-03-27 10:48:01 +0800

    首页=》店铺列表

--- sfddj-miniApp-ali/community/pages/components/ShopList/ShopList.js
+++ sfddj-miniApp-ali/community/pages/components/ShopList/ShopList.js
@@ -1,92 +1,30 @@
+import Shop from '/community/service/shop';
+
 Component({
   mixins: [],
   data: {
-    list: [
-      {
-        "address": "皇后大道东",
-        "city": "深圳",
-        "id": 1,
-        "latitude": 10,
-        "longitude": 10,
-        "distance": 1000, // 距离
-        "notice": "string",
-        "province": "string",
-        "servicePhone": "123456",
-        "shopGoodsList": [
-          {
-            "goodInfo": "xxxxxxxxxx",
-            "goodsImagePath": "/community/assets/images/a.png",
-            "goodsSn": "sn10",
-            "id": 100,
-            "introduction": "string",
-            "isTuangou": false,
-            "monthSaleCount": 0,
-            "orderList": 0,
-            "priceShow": "160", // 展示价格
-            "score": 0,
-            "shopCategoryId": 0,
-            "shopGoodsSkuList": [
-              {
-                "discountPrice": 0, // 折扣价格
-                "freezeStore": 0,
-                "goodsId": 0,
-                "iavSort": 0,
-                "iavStatus": 0,
-                "iavValue": "string",
-                "id": 0,
-                "isDiscount": false,
-                "salePrice": 0, // 销售价格
-                "store": 0
-              }
-            ],
-            "title": "正宗新疆阿克苏密梨香甜可口人见人爱正宗新疆阿克苏密梨香甜可口人见人爱",
-            "totalSaleCount": 0,
-            "xgCount": 0
-          },
-          {
-            "goodInfo": "xxxxxxxxxx",
-            "goodsImagePath": "/community/assets/images/a.png",
-            "goodsSn": "sn10",
-            "id": 101,
-            "introduction": "string",
-            "isTuangou": false,
-            "monthSaleCount": 0,
-            "orderList": 0,
-            "priceShow": "280", // 展示价格
-            "score": 0,
-            "shopCategoryId": 0,
-            "shopGoodsSkuList": [
-              {
-                "discountPrice": 0, // 折扣价格
-                "freezeStore": 0,
-                "goodsId": 0,
-                "iavSort": 0,
-                "iavStatus": 0,
-                "iavValue": "string",
-                "id": 0,
-                "isDiscount": false,
-                "salePrice": 0, // 销售价格
-                "store": 0
-              }
-            ],
-            "title": "正宗西藏地瓜可口人见人爱正宗西藏地瓜可口人见人爱",
-            "totalSaleCount": 0,
-            "xgCount": 0
-          }
-        ],
-        "shopLogo": "/community/assets/images/a.png",
-        "shopName": "宋小宝美食天地",
-        "supplierId": 0
-      }
-      // {id: 10, tit: '正宗新疆阿克苏密梨香甜可口人见人爱正宗新疆阿克苏密梨香甜可口人见人爱', price: 100, sprice: 160, img:'/community/assets/images/a.png'},
-      // {id: 20, tit: '正宗西藏地瓜香甜可口人见人爱正宗西藏地瓜香甜可口人见人爱', price: 260, sprice: 290, img:'/community/assets/images/b.png'}
-    ]
+    list: []
   },
   props: {},
   didMount() {
-
+    this.shop = Shop.init('shop', this);
+    this.nextPageIdx = 0;
+    this.loadMore();
   },
   didUpdate() {},
   didUnmount() {},
-  methods: {},
+  methods: {
+    loadMore() {
+      this.shop.gets(11, 22, this.nextPageIdx, (res) => {
+        if(res.data && res.data.data) {
+          if(res.data.data.length) {
+            this.nextPageIdx++;
+          }
+          this.$spliceData({
+            list: [this.data.list.length, 0, ...res.data.data]
+          });
+        }
+      });
+    }
+  },
 });
--- sfddj-miniApp-ali/community/service/shop.js
+++ sfddj-miniApp-ali/community/service/shop.js
@@ -14,12 +14,34 @@ class Shop extends MiniAppService {
     }
 
     // 取得指定经纬度附近的所有店铺
-    gets(x, y, callbackFun) {
+    gets(x, y, pageIdx, callbackFun) {
+        let pageSize = 10;
         http.get(api.Shop.search, {
             longitude: x,
-            latitude: y
+            latitude: y,
+            start: pageIdx * pageSize,
+            limit: pageSize
         }, (res) => {
-            console.log('WWW', res);
+            if(res.data && res.data.data) {
+                res.data.data.forEach((T) => {
+                    T.shopLogo = api.baseImageUrl + T.shopLogo;
+                    if(T.shopGoodsList) {
+                        T.shopGoodsList.forEach((M) => {
+                            if(M.goodsImagePath) {
+                                M.goodsImagePath = JSON.parse(M.goodsImagePath);
+                                for(let i = 0; i < M.goodsImagePath.length; i++) {
+                                    M.goodsImagePath[i] = api.baseImageUrl + M.goodsImagePath[i];
+                                }
+                            } else {
+                                M.goodsImagePath = [];
+                            }
+                        });
+                    }
+                });
+            }
+            if(callbackFun) {
+                callbackFun(res);
+            }
         });
     }
 }
