803ce0aadf1816b53e8ca536e9dcbfcde1210d07
caoyongfeng <HxKgnx7saX>
2020-03-27 19:23:13 +0800

    店铺详情。 购物车。

--- sfddj-miniApp-ali/community/service/shop.js
+++ sfddj-miniApp-ali/community/service/shop.js
@@ -16,7 +16,7 @@ class Shop extends MiniAppService {
     // 取得指定经纬度附近的所有店铺
     gets(x, y, pageIdx, callbackFun) {
         let pageSize = 10;
-        http.get(api.Shop.search, {
+        http.get(api.Shop.SEARCH, {
             longitude: x,
             latitude: y,
             start: pageIdx * pageSize,
@@ -36,6 +36,50 @@ class Shop extends MiniAppService {
                                 M.goodsImagePath = [];
                             }
                         });
+                    }
+                });
+            }
+            if(callbackFun) {
+                callbackFun(res);
+            }
+        });
+    }
+
+
+    get(id, callbackFun) {
+        http.get(api.Shop.GETBYID + id, {}, (res) => {
+            if(res.data && res.data.data) {
+                res.data.data.shopLogo = api.baseImageUrl + res.data.data.shopLogo;
+            }
+            if(callbackFun) {
+                callbackFun(res);
+            }
+        });
+    }
+
+
+    // 取指定店铺的所有商品类别
+    getCategories(shopId, callbackFun) {
+        http.get(api.Shop.GETCATEGORIES + shopId, {}, (res) => {
+            if(callbackFun) {
+                callbackFun(res);
+            }
+        });
+    }
+
+
+    // 取指定店铺的商品列表
+    getGoodsOfShop(shopId, pageIdx, callbackFun) {
+        http.get(api.Shop.GETGOODSOFSHOP + shopId, {}, (res) => {
+            if(res.data && res.data.data) {
+                res.data.data.forEach((T) => {
+                    if(T.goodsImagePath) {
+                        T.goodsImagePath = JSON.parse(T.goodsImagePath);
+                        for(let i = 0; i < T.goodsImagePath.length; i++) {
+                            T.goodsImagePath[i] = api.baseImageUrl + T.goodsImagePath[i];
+                        }
+                    } else {
+                        T.goodsImagePath = [];
                     }
                 });
             }
