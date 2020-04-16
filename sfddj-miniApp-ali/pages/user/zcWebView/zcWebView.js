052ec22b86946ef9dad24a2628647a25cbe50f1e
楼小楼 <13266521327@163.com>
2020-04-16 18:42:17 +0800

    智齿增加跳转链接参数

--- sfddj-miniApp-ali/pages/user/zcWebView/zcWebView.js
+++ sfddj-miniApp-ali/pages/user/zcWebView/zcWebView.js
@@ -3,7 +3,7 @@ Page({
   onLoad: function (options) {
       this.setData({
         link: options.link,
-        link: `https://sfddj.sobot.com/chat/h5/v2/index.html?sysnum=7a02fc9cf20d4c59947a5937837851c1&uname=` + options.uname
+        link: `https://sfddj.sobot.com/chat/h5/v2/index.html?sysnum=7a02fc9cf20d4c59947a5937837851c1&uname=` + options.uname + 'groupid=a9b3ae2b38dd4a80bbb7a7669fb36919'
       });
   },
 });
